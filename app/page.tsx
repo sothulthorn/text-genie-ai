797f38c8d5e046094b26abe9b791214a5e54fc08
sothulthorn <thornsothul@gmail.com>
2024-09-03 21:40:10 +0900

    Setup Google Gemini Generative AI

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
-import Image from 'next/image';
+'use client';
 
-export default function Home() {
-  return <main>Hello from Next</main>;
+import { useState } from 'react';
+
+import { runAi } from '@/actions/ai';
+
+export default function Page() {
+  const [response, setResponse] = useState('');
+  const [isLoading, setLoading] = useState(false);
+
+  const handleClick = async () => {
+    setLoading(true);
+
+    try {
+      const data = await runAi('write a short zen story');
+      setResponse(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <button onClick={handleClick}>Run AI</button>
+      <hr />
+      <div>{isLoading ? 'Loading...' : response}</div>
+    </>
+  );
 }
